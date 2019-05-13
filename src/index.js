a48cfa3eb7b005d0a4eeb566d2980cda1a2de525
yzqi <yzqi@thoughtworks.com>
2019-05-13 17:13:16 +0800

    react init.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('webpack set up!');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const title = "react webpack babel set up";
+
+ReactDom.render(
+    <div>{title}</div>,
+document.getElementById('app')
+)
