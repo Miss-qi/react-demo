9be86494bcc1af57d359a4360e31d3f0b8994307
yzqi <yzqi@thoughtworks.com>
2019-06-21 09:26:18 +0800

    add plugin for babel

---
+++ src/components/ControlPanel.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Counter from './Counter';
+
+const style = {
+    margin: '20px'
+};
+
+class ControlPanel extends Component {
+    constructor(props) {
+        super(props);
+        this.initValues = [0, 10, 20];
+        const initSum = this.initValues.reduce((a, b) => a + b, 0);
+        this.state = {
+            sum: initSum
+        }
+    }
+
+    onUpdate = (newValue, previousValue) => {
+        const valueChange = newValue - previousValue;
+        this.setState({ sum: this.state.sum + valueChange });
+    }
+
+    render() {
+        console.log('enter ControlPanel render');
+
+        return(
+            <div style={style}>
+                <Counter caption="First" onUpdate={this.onUpdate}/>
+                <Counter caption="Second" initValue={10} onUpdate={this.onUpdate}/>
+                <Counter caption="Third" initValue={20} onUpdate={this.onUpdate}/>
+                <button onClick={ () => this.forceUpdate() }>
+                    Click me to re-render!
+                </button>
+                <div>Total Count: {this.state.sum}</div>
+            </div>
+        )
+    }
+}
+
+export default ControlPanel;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDom from 'react-dom';
 import Welcome from './components/Welcome';
 import Example from './components/Example';
 import { Counter } from "./components/Example";
+import ControlPanel from './components/ControlPanel';
 
 const title = "react webpack babel set up";
 const element = <div>{title}</div>;
 
 ReactDom.render(<div>
-    <Welcome name="test"/>
-    <Example/>
-    <Counter initialCount={0}/>
+    {/*<Welcome name="test"/>*/}
+    {/*<Example/>*/}
+    <ControlPanel/>
+    {/*<Counter initialCount={0}/>*/}
 </div>, document.getElementById('app'));
 
 module.hot.accept();
