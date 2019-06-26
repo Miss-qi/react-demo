f2727a873151a7e75512539b0bd7e40de1a7bed1
yzqi <yzqi@thoughtworks.com>
2019-06-26 15:53:45 +0800

    add transition.

--- src/todos/views/todoList.js
+++ src/todos/views/todoList.js
@@ -1,25 +1,31 @@
 import React from "react";
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
+import TransitionGroup from 'react-addons-css-transition-group';
 import TodoItem from './todoItem';
 import { removeTodo, toggleTodo } from "../actions";
-import { FilterTypes } from "../../contants";
 import { selectVisibleTodos } from "../selector";
 
 const TodoList = ({ todos, onToggleTodo, onRemoveTodo }) => {
     return (
         <ul>
-            {
-                todos.map(item => (
-                    <TodoItem
-                        key={item.id}
-                        text={item.text}
-                        completed={item.completed}
-                        onToggle={() => onToggleTodo(item.id)}
-                        onRemove={() => onRemoveTodo(item.id)}
-                    />
-                ))
-            }
+            <TransitionGroup
+                transitionName="fade"
+                transitionEnterTimeout={500}
+                transitionLeaveTimeout={500}
+            >
+                {
+                    todos.map(item => (
+                        <TodoItem
+                            key={item.id}
+                            text={item.text}
+                            completed={item.completed}
+                            onToggle={() => onToggleTodo(item.id)}
+                            onRemove={() => onRemoveTodo(item.id)}
+                        />
+                    ))
+                }
+            </TransitionGroup>
         </ul>
     )
 }
